da25c7198717d973fb87cadfb99ed05016cefe51
Vincent Emonet <vincent.emonet@gmail.com>
2020-11-06 08:53:56 +0100

    Updates

---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/shapes-of-you/expo-service-worker.js",{scope:"/shapes-of-you/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
