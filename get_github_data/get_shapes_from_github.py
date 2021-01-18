3d4b85be9d7f1a3c11ee665d3ec78d3044d43592
Vincent Emonet <vincent.emonet@gmail.com>
2021-01-18 02:34:51 +0100

    refactor python script

--- get_github_data/get_shapes_from_github.py
+++ get_github_data/get_shapes_from_github.py
@@ -1,4 +1,3 @@
-from logging import exception
 from python_graphql_client import GraphqlClient
 import pathlib
 import os
@@ -7,6 +6,7 @@ import urllib
 from rdflib import Graph, plugin, Literal, RDF, URIRef, Namespace
 from rdflib.serializer import Serializer
 from rdflib.namespace import RDFS, XSD, DC, DCTERMS, VOID
+# from logging import exception
 
 SPARQL_ENDPOINT_URL='https://graphdb.dumontierlab.com/repositories/shapes-registry'
 SPARQL_ENDPOINT_UPDATE_URL='https://graphdb.dumontierlab.com/repositories/shapes-registry/statements'
@@ -20,17 +20,19 @@ SH = Namespace("http://www.w3.org/ns/shacl#")
 SCHEMA = Namespace("https://schema.org/")
 
 def main():
+  client = GraphqlClient(endpoint="https://api.github.com/graphql")
+
   # Reset failed shapes report file
   with open(root / '../FAILED_IMPORT_REPORT.md', 'w') as f:
     f.write('## Fails loading files to `rdflib`\n' +
       '*Please check if your RDF file is properly formatted. We recommend to **use https://www.easyrdf.org/converter to get better insights on the error**, and store the shapes in `.ttl` files*\n\n\n')
 
-  # shapes_graph = Graph()
-  shapes_graph = fetch_shape_files(TOKEN)
-  fetch_extra_shape_files(TOKEN, shapes_graph)
+  shapes_graph = Graph()
+  shapes_graph = fetch_shape_files(shapes_graph, client, TOKEN)
+  shapes_graph = fetch_extra_shape_files(shapes_graph, client, TOKEN)
 
-  # insert_graph_in_sparql_endpoint(shapes_graph)
   shapes_graph.serialize('shapes-of-you-rdf.ttl', format='turtle')
+
 
 def generate_github_file_url(repo_url, filepath, branch):
   """GitHub does not provide a way to get the download URL directly from GraphQL
@@ -39,6 +41,82 @@ def generate_github_file_url(repo_url, filepath, branch):
   file_url = repo_url.replace("https://github.com/", "https://raw.githubusercontent.com/")
   file_url += '/' + branch + '/' + urllib.parse.quote_plus(filepath)
   return file_url
+
+def get_files(extensions):
+    """List all files with given extensions in subfolders
+    :param extensions: Array of extensions, e.g. .ttl, .rdf
+    """
+    all_files = []
+    for ext in extensions:
+        all_files.extend(pathlib.Path('cloned_repo').rglob(ext))
+    return all_files
+
+
+def process_shapes_file(shape_format, shapes_graph, rdf_file_path, repo_url, branch, repo_description):
+    """Process a Shapes file, check its content and add entry to the shapes graph
+    """
+    relative_filepath = str(rdf_file_path)[12:]
+    github_file_url = generate_github_file_url(repo_url, relative_filepath, branch)
+    g = Graph()
+    if shape_format == 'shex':
+      # no parsing possible for shex
+      file_uri = URIRef(github_file_url)
+      shapes_graph.add((file_uri, RDF.type, SCHEMA['DataDownload']))
+      shapes_graph.add((file_uri, RDFS.label, Literal(rdf_file_path.name)))
+      shapes_graph.add((file_uri, DCTERMS.hasPart, Literal('ShEx model')))
+      shapes_graph.add((file_uri, DC.source, URIRef(repo_url)))
+      if (repo_description):
+        shapes_graph.add((URIRef(repo_url), RDFS.comment, Literal(repo_description)))
+    else:
+      try:
+          g.parse(str(rdf_file_path.absolute()), format=shape_format)
+      except Exception as e:
+          print('No parser worked for the file ' + github_file_url)
+          if not str(rdf_file_path).endswith('.xml') and not str(rdf_file_path).endswith('.json'):
+              with open(root / '../FAILED_IMPORT_REPORT.md', 'a') as f:
+                f.write('File: ' + github_file_url + "\n\n"
+                    + 'In repository: ' + repo_url + "\n> " 
+                    + str(e) + "\n\n---\n")
+
+      for shape in g.subjects(RDF.type, SH.NodeShape):
+          file_uri = URIRef(github_file_url)
+          shapes_graph.add((file_uri, RDF.type, SCHEMA['DataDownload']))
+          shapes_graph.add((file_uri, RDFS.label, Literal(rdf_file_path.name)))
+          shapes_graph.add((file_uri, DC.source, URIRef(repo_url)))
+          shape_label = shape
+          for label in g.objects(shape, RDFS.label):
+              # Try to get the label of the shape
+              shape_label = label
+          shapes_graph.add((file_uri, DCTERMS.hasPart, Literal(shape_label)))
+
+    return shapes_graph
+
+def clone_and_process_repo(shapes_graph, repo_url, branch, repo_description):
+    print('Cloning ' + repo_url)
+    os.system('git clone --quiet --depth 1 --recurse-submodules --shallow-submodules ' + repo_url + ' cloned_repo')
+    # os.chdir('cloned_repo') # Specifying the path where the cloned project needs to be copied
+
+    for rdf_file_path in get_files(['*.shex']):
+        shapes_graph = process_shapes_file('shex', shapes_graph, rdf_file_path, repo_url, branch, repo_description)
+
+    for rdf_file_path in get_files(['*.trig', '*.n3']):
+        shapes_graph = process_shapes_file('n3', shapes_graph, rdf_file_path, repo_url, branch, repo_description)
+
+    for rdf_file_path in get_files(['*.json', '*.jsonld', '*.json-ld']):
+        shapes_graph = process_shapes_file('json-ld', shapes_graph, rdf_file_path, repo_url, branch, repo_description)
+
+    for rdf_file_path in get_files(['*.xml', '*.rdf']):
+        shapes_graph = process_shapes_file('xml', shapes_graph, rdf_file_path, repo_url, branch, repo_description)
+
+    for rdf_file_path in get_files(['*.ttl', '*.shacl']):
+        shapes_graph = process_shapes_file('ttl', shapes_graph, rdf_file_path, repo_url, branch, repo_description)
+
+    for rdf_file_path in get_files(['*.nt']):
+        shapes_graph = process_shapes_file('nt', shapes_graph, rdf_file_path, repo_url, branch, repo_description)
+        
+    shutil.rmtree('cloned_repo', ignore_errors=True, onerror=None)
+    return shapes_graph
+
 
 # Get all shapes for all repos with shacl-shapes tag
 def get_shapes_query(github_topic, after_cursor=None):
@@ -65,79 +143,11 @@ query {
   }   
 }
 """.replace(
-        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
-    )
-
-def get_files(extensions):
-    all_files = []
-    for ext in extensions:
-        all_files.extend(pathlib.Path('cloned_repo').rglob(ext))
-    return all_files
-
-
-def process_shapes_file(shape_format, shapes_graph, rdf_file_path, repo_url, branch):
-    relative_filepath = str(rdf_file_path)[12:]
-    github_file_url = generate_github_file_url(repo_url, relative_filepath, branch)
-    g = Graph()
-    if shape_format == 'shex':
-      # no parsing possible for shex
-      file_uri = URIRef(github_file_url)
-      shapes_graph.add((file_uri, RDF.type, SCHEMA['DataDownload']))
-      shapes_graph.add((file_uri, RDFS.label, Literal(rdf_file_path.name)))
-      shapes_graph.add((file_uri, DC.source, URIRef(repo_url)))
-      shapes_graph.add((file_uri, DCTERMS.hasPart, Literal('ShEx model')))
-    else:
-      try:
-          g.parse(str(rdf_file_path.absolute()), format=shape_format)
-      except Exception as e:
-          print('No parser worked for the file ' + github_file_url)
-          if not str(rdf_file_path).endswith('.xml') and not str(rdf_file_path).endswith('.json'):
-              with open(root / '../FAILED_IMPORT_REPORT.md', 'a') as f:
-                f.write('File: ' + github_file_url + "\n\n"
-                    + 'In repository: ' + repo_url + "\n> " 
-                    + str(e) + "\n\n---\n")
-
-      for shape in g.subjects(RDF.type, SH.NodeShape):
-          file_uri = URIRef(github_file_url)
-          shapes_graph.add((file_uri, RDF.type, SCHEMA['DataDownload']))
-          shapes_graph.add((file_uri, RDFS.label, Literal(rdf_file_path.name)))
-          shapes_graph.add((file_uri, DC.source, URIRef(repo_url)))
-          shape_label = shape
-          for label in g.objects(shape, RDFS.label):
-              # Try to get the label of the shape
-              shape_label = label
-          shapes_graph.add((file_uri, DCTERMS.hasPart, Literal(shape_label)))
-
-    return shapes_graph
-
-def clone_and_process_repo(shapes_graph, repo_url, branch):
-    os.system('git clone --quiet --depth 1 --recurse-submodules --shallow-submodules ' + repo_url + ' cloned_repo')
-    # os.chdir('cloned_repo') # Specifying the path where the cloned project needs to be copied
-
-    for rdf_file_path in get_files(['*.shex']):
-        shapes_graph = process_shapes_file('shex', shapes_graph, rdf_file_path, repo_url, branch)
-
-    for rdf_file_path in get_files(['*.trig', '*.n3']):
-        shapes_graph = process_shapes_file('n3', shapes_graph, rdf_file_path, repo_url, branch)
-
-    for rdf_file_path in get_files(['*.json', '*.jsonld', '*.json-ld']):
-        shapes_graph = process_shapes_file('json-ld', shapes_graph, rdf_file_path, repo_url, branch)
-
-    for rdf_file_path in get_files(['*.xml', '*.rdf']):
-        shapes_graph = process_shapes_file('xml', shapes_graph, rdf_file_path, repo_url, branch)
-
-    for rdf_file_path in get_files(['*.ttl', '*.shacl']):
-        shapes_graph = process_shapes_file('ttl', shapes_graph, rdf_file_path, repo_url, branch)
-
-    for rdf_file_path in get_files(['*.nt']):
-        shapes_graph = process_shapes_file('nt', shapes_graph, rdf_file_path, repo_url, branch)
-        
-    shutil.rmtree('cloned_repo', ignore_errors=True, onerror=None)
-    return shapes_graph
+    "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
+)
 
 # Retrieve releases in projects returned by the GraphQL calls
-def fetch_shape_files(oauth_token):
-    shapes_graph = Graph()
+def fetch_shape_files(shapes_graph, client, oauth_token):
     topics = ['shacl-shapes', 'shex']
 
     for github_topic in topics:
@@ -153,8 +163,9 @@ def fetch_shape_files(oauth_token):
               repo_json = repository["repo"]
               repo_url = repo_json["url"]
               branch = repo_json['defaultBranchRef']['name']
-              print(repo_url)
-              shapes_graph = clone_and_process_repo(shapes_graph, repo_url, branch)
+              repo_description = repo_json["description"]
+              # repo_description = repo_json["shortDescriptionHTML"]
+              shapes_graph = clone_and_process_repo(shapes_graph, repo_url, branch, repo_description)
                   
           has_next_page = data["data"]["search"]["pageInfo"][
               "hasNextPage"
@@ -181,11 +192,14 @@ def get_extra_graphql_query(repo):
     }
   }'''
 
-def fetch_extra_shape_files(oauth_token, shapes_graph):
+def fetch_extra_shape_files(shapes_graph, client, oauth_token, extra_shapes_repositories):
   """Fetch additional Shapes files from a list of GitHub repos
   """
-  print('extra_shapes_repositories')
-  print(extra_shapes_repositories)
+  extra_shapes_repositories = []
+  with open(root / '../EXTERNAL_REPOSITORIES.txt', 'r') as f:
+    for line in f:
+      extra_shapes_repositories.append(line.rstrip('\n'))
+
   for extra_repo in extra_shapes_repositories:
     data = client.execute(
         query=get_extra_graphql_query(extra_repo),
@@ -193,28 +207,28 @@ def fetch_extra_shape_files(oauth_token, shapes_graph):
     )
     # print(json.dumps(data, indent=4))
     repo_json = data["data"]["repository"]
-    try:
-      branch = repo_json['defaultBranchRef']['name']
-      repo_url = repo_json["url"]
-      shapes_graph = clone_and_process_repo(shapes_graph, repo_url, branch)
-    except Exception as e:
-      print('FAIL')
-      print(e)
+    # try:
+    branch = repo_json['defaultBranchRef']['name']
+    repo_url = repo_json["url"]
+    repo_description = repo_json["description"]
+    shapes_graph = clone_and_process_repo(shapes_graph, repo_url, branch, repo_description)
+    # except Exception as e:
+    #   print('FAIL')
+    #   print(e)
   return shapes_graph
 
 
 if __name__ == "__main__":
-  # Scripts starts here
-  root = pathlib.Path(__file__).parent.resolve()
+  # The script starts here
   root = pathlib.Path(__file__).parent.resolve()
 
-  global extra_shapes_repositories
-  extra_shapes_repositories = []
-  with open(root / '../EXTERNAL_REPOSITORIES.txt', 'r') as f:
-    for line in f:
-      extra_shapes_repositories.append(line.rstrip('\n'))
+  # global extra_shapes_repositories
+  # extra_shapes_repositories = []
+  # with open(root / '../EXTERNAL_REPOSITORIES.txt', 'r') as f:
+  #   for line in f:
+  #     extra_shapes_repositories.append(line.rstrip('\n'))
 
-  client = GraphqlClient(endpoint="https://api.github.com/graphql")
+  # client = GraphqlClient(endpoint="https://api.github.com/graphql")
 
   main()
 
