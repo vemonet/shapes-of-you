07ac3dc1303a7767300f5ef8de8781ca21375470
Vincent Emonet <vincent.emonet@gmail.com>
2021-02-24 20:40:05 +0100

    improve loader

--- etl/index_shapes.py
+++ etl/index_shapes.py
@@ -364,7 +364,6 @@ def clone_and_process_repo(shapes_graph, repo_url, branch, repo_description, git
     # curl -T shapes-rdf.ttl  https://data.index.semanticscience.org/DAV/home/dba/rdf_sink/$GIT_SERVICE.ttl -u dba:password
     # https://github.com/MaastrichtU-IDS/shapes-of-you becomes: github.com/MaastrichtU-IDS/shapes-of-you.ttl
     if (None, None, None) in shapes_graph:
-      print("This graph contains triples about Bob!")
       shapes_graph.serialize('shapes-rdf.ttl', format='turtle')
 
       repo_vad_url = repo_url.replace('https://', '').replace('http://', '')
@@ -375,7 +374,7 @@ def clone_and_process_repo(shapes_graph, repo_url, branch, repo_description, git
         print(f.read())
 
       rdf_file_path = root / '../shapes-rdf.ttl'
-      os.system('curl -T ' + rdf_file_path + ' https://data.index.semanticscience.org/DAV/home/dba/rdf_sink/' + repo_vad_url + '.ttl -u dba:' + ENDPOINT_PASSWORD)
+      os.system('curl -T ' + str(rdf_file_path) + ' https://data.index.semanticscience.org/DAV/home/dba/rdf_sink/' + repo_vad_url + '.ttl -u dba:' + ENDPOINT_PASSWORD)
       # To delete:
       # select DB.DBA.DAV_DELETE ('/DAV/home/dba/rdf_sink/gitee.ttl', 0, 'dba', 'dba');
       try:
