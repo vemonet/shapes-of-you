c1ef89209b2228a496c7aea82139b895fe1b673f
Vincent Emonet <vincent.emonet@gmail.com>
2021-04-20 00:33:04 +0200

    update reset_virtuoso

--- reset_virtuoso.sh
+++ reset_virtuoso.sh
@@ -12,8 +12,8 @@ docker exec -it shapes-of-you-virtuoso apt update
 docker exec -it shapes-of-you-virtuoso apt install -y wget
 
 docker exec -it shapes-of-you-virtuoso wget -O /usr/local/virtuoso-opensource/share/virtuoso/vad/ods_framework_dav.vad http://download3.openlinksw.com/uda/vad-vos-packages/7.2/ods_framework_dav.vad
-wget -O /usr/local/virtuoso-opensource/share/virtuoso/vad/ods_briefcase_dav.vad http://download3.openlinksw.com/uda/vad-vos-packages/7.2/ods_briefcase_dav.vad
-isql-v -U dba -P $ENDPOINT_PASSWORD exec="vad_install ('/usr/local/virtuoso-opensource/share/virtuoso/vad/ods_framework_dav.vad', 0)"
-isql-v -U dba -P $ENDPOINT_PASSWORD exec="vad_install ('/usr/local/virtuoso-opensource/share/virtuoso/vad/ods_briefcase_dav.vad', 0)"
+docker exec -it shapes-of-you-virtuoso wget -O /usr/local/virtuoso-opensource/share/virtuoso/vad/ods_briefcase_dav.vad http://download3.openlinksw.com/uda/vad-vos-packages/7.2/ods_briefcase_dav.vad
+docker exec -it shapes-of-you-virtuoso isql-v -U dba -P $ENDPOINT_PASSWORD exec="vad_install ('/usr/local/virtuoso-opensource/share/virtuoso/vad/ods_framework_dav.vad', 0)"
+docker exec -it shapes-of-you-virtuoso isql-v -U dba -P $ENDPOINT_PASSWORD exec="vad_install ('/usr/local/virtuoso-opensource/share/virtuoso/vad/ods_briefcase_dav.vad', 0)"
 
 # docker exec -it shapes-of-you-virtuoso isql-v -U dba -P $ENDPOINT_PASSWORD exec="vad_install ('/usr/local/virtuoso-opensource/share/virtuoso/vad/ods_framework_dav.vad', 0)"
