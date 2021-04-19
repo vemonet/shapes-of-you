c4ae734b582d7beb6099617c266c0e9aa93f621f
Vincent Emonet <vincent.emonet@gmail.com>
2021-04-20 00:36:18 +0200

    update reset_virtuoso

--- reset_virtuoso.sh
+++ reset_virtuoso.sh
@@ -11,9 +11,9 @@ wait 10
 docker exec -it shapes-of-you-virtuoso apt update
 docker exec -it shapes-of-you-virtuoso apt install -y wget
 
-docker exec -it shapes-of-you-virtuoso wget -O /usr/local/virtuoso-opensource/share/virtuoso/vad/ods_framework_dav.vad http://download3.openlinksw.com/uda/vad-vos-packages/7.2/ods_framework_dav.vad
-docker exec -it shapes-of-you-virtuoso wget -O /usr/local/virtuoso-opensource/share/virtuoso/vad/ods_briefcase_dav.vad http://download3.openlinksw.com/uda/vad-vos-packages/7.2/ods_briefcase_dav.vad
-docker exec -it shapes-of-you-virtuoso isql-v -U dba -P $ENDPOINT_PASSWORD exec="vad_install ('/usr/local/virtuoso-opensource/share/virtuoso/vad/ods_framework_dav.vad', 0)"
-docker exec -it shapes-of-you-virtuoso isql-v -U dba -P $ENDPOINT_PASSWORD exec="vad_install ('/usr/local/virtuoso-opensource/share/virtuoso/vad/ods_briefcase_dav.vad', 0)"
+docker exec -it shapes-of-you-virtuoso wget -O /opt/virtuoso-opensource/vad/ods_framework_dav.vad http://download3.openlinksw.com/uda/vad-vos-packages/7.2/ods_framework_dav.vad
+docker exec -it shapes-of-you-virtuoso wget -O /opt/virtuoso-opensource/vad/ods_briefcase_dav.vad http://download3.openlinksw.com/uda/vad-vos-packages/7.2/ods_briefcase_dav.vad
+docker exec -it shapes-of-you-virtuoso isql-v -U dba -P $ENDPOINT_PASSWORD exec="vad_install ('/opt/virtuoso-opensource/vad/ods_framework_dav.vad', 0)"
+docker exec -it shapes-of-you-virtuoso isql-v -U dba -P $ENDPOINT_PASSWORD exec="vad_install ('/opt/virtuoso-opensource/vad/ods_briefcase_dav.vad', 0)"
 
-# docker exec -it shapes-of-you-virtuoso isql-v -U dba -P $ENDPOINT_PASSWORD exec="vad_install ('/usr/local/virtuoso-opensource/share/virtuoso/vad/ods_framework_dav.vad', 0)"
+# docker exec -it shapes-of-you-virtuoso isql-v -U dba -P $ENDPOINT_PASSWORD exec="vad_install ('/opt/virtuoso-opensource/vad/ods_framework_dav.vad', 0)"
